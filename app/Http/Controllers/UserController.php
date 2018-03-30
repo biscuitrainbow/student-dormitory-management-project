fcc9f06970cfad1a6a8409cf4c5f7905c1793725
Natthapon Sricort <Natthaponsricort@gmail.com>
2018-03-31 00:32:35 +0700

    Fix signup

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,67 +8,77 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function index(){
-        $users = User::all(); 
-        return view('user-index',compact('users'));
+    public function index()
+    {
+        $users = User::all();
+        return view('user-index', compact('users'));
     }
 
-    public function login(){
+    public function login()
+    {
         return view('login');
     }
 
-    public function authenticate(){
+    public function authenticate()
+    {
 
-        if(Auth::attempt(['username' =>request('username'),'password' => request('password')])){
+        if (Auth::attempt(['username' => request('username'), 'password' => request('password')])) {
             return redirect('\dashboard');
         }
 
         return redirect()->back();
-    
+
     }
-    
-    public function logout(){
+
+    public function logout()
+    {
         Auth::logout();
 
         return redirect('\login');
     }
 
-    public function signup(){
+    public function signup()
+    {
         return view('signup');
     }
 
-    public function delete(User $user){
+    public function delete(User $user)
+    {
         $user->delete();
         return redirect('/user/index');
-        
+
     }
 
 
-    public function edit(User $user){
-        return view('user-edit',compact('user'));   
+    public function edit(User $user)
+    {
+        return view('user-edit', compact('user'));
     }
 
-    
 
-    public function update(User $user){
+
+    public function update(User $user)
+    {
         $user->update([
             'first_name' => request()->first_name,
             'last_name' => request()->last_name,
-            'username'=>request()->username,
+            'username' => request()->username,
         ]);
         return redirect('/user/index');
     }
-    
-    public function create(){
+
+    public function create()
+    {
         return view('user-create');
     }
 
-    public function store(){
-     User::create([
+    public function store()
+    {
+        User::create([
             'first_name' => request()->first_name,
             'last_name' => request()->last_name,
             'username' => request()->username,
-            'password'=> bcrypt(request()->pass),
+            'password' => bcrypt(request()->pass),
         ]);
 
         return redirect('/user/index');
